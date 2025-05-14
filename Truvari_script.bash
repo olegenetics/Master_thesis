a1656fbbea456aa5746341dd5e6da23292923e04
olegenetics <olemariusgr@gmail.com>
2025-05-14 23:28:32 +0200

    Update Truvari_script.bash

--- Truvari_script.bash
+++ Truvari_script.bash
@@ -11,6 +11,6 @@ module load Miniconda3 && eval "$(conda shell.bash hook)"
 conda activate /mnt/users/oleg/.conda/envs/mapping_and_calling
 
 echo "Working with this $CONDA_PREFIX environment ..."
-
+The bench paramater with the options -b (base) and -c (comparison) file were used to compared the "truth" sample of SVs with the downsampled datasets
 truvari bench -b /mnt/users/oleg/vcf/70x_before_filtering.vcf.gz -c /mnt/users/oleg/vcf/10x_seed1_after_filtering.vcf.gz -o /mnt/SCRATCH/oleg/fastq/truvari_70x_10x_seed1_after_filterng
 
