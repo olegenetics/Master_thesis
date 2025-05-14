8bdc226b6c6af83382a1fb8ba4ad0adca55bcb51
olegenetics <olemariusgr@gmail.com>
2025-05-14 23:25:40 +0200

    Update Rasusa_script.bash

--- Rasusa_script.bash
+++ Rasusa_script.bash
@@ -15,7 +15,7 @@ conda activate /mnt/users/oleg/.conda/envs/MyCondaEnvironments
 echo "Working with this $CONDA_PREFIX environment ..."
 
 
-## run rasusa
+## runs rasusa by applying desired read depth and size of the reference genome
 
 rasusa reads -c 40 -g 2.6gb --seed 1 /mnt/SCRATCH/oleg/fastq/flakong.start.fastq.gz | gzip > /mnt/SCRATCH/oleg/fastq/downsampling/seed1__flakong_40x.fastq.gz
 rasusa reads -c 40 -g 2.6gb --seed 2 /mnt/SCRATCH/oleg/fastq/flakong.start.fastq.gz | gzip > /mnt/SCRATCH/oleg/fastq/downsampling/seed2__flakong_40x.fastq.gz
